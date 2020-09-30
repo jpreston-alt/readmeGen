76e4e73aa37ecec435bd1904fb129f549c24ce7b
Joanna Preston <joannappreston@gmail.com>
2020-09-30 14:42:35 -0700

    pseudocoded readme generator homework

---
+++ index.js
@@ -0,0 +1,17 @@
+// npm init
+// install my dependencies - inquirer
+
+// require all the dependencies I'll need - inquirer, fs
+
+// create an array of questions
+
+// writing a README in a markdown file as a template
+
+// function that will generate my readme template
+
+// use inquirer to prompt users with questions
+
+// use answers that come back from inquirer - pass those into my generate readme function
+
+// write file using template generated from readme function
+
